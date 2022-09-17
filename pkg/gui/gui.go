75ab0a701cdf4700aacd84ad83766cb976f37e49
Gabe <32711718+gowhale@users.noreply.github.com>
2022-09-17 21:10:21 +0100

    bug(gui): When screen too small can't select button (#41)

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -14,11 +14,18 @@ import (
 
 const (
 	screenWidth       = 600
-	screenHeight      = 750
-	recipeListHeight  = 700
+	screenHeight      = 500
 	progressBarHeight = 50
+	submitBarHeight   = 50
+	recipeListHeight  = screenHeight - progressBarHeight - submitBarHeight
 	progressBarEmpty  = 0.0
 	progressBarFull   = 1.0
+
+	listContainerCols = 1
+	listCols          = 2
+	listBoxIndex      = 0
+	checkIndex        = 0
+	labelIndex        = 1
 )
 
 type screen struct {
@@ -36,13 +43,21 @@ func (s *screen) UpdateLabel(msg string) {
 	s.l.Refresh()
 }
 
-func createSubmitButton(s common.ScreenInterface, wf common.WorkflowInterface, fr recipe.FileReader, recipes *[]string, recipeMap map[string]recipe.Recipe) *widget.Button {
-	return widget.NewButton("Add To Shopping List", func() {
-		err := wf.SubmitShoppingList(s, wf, fr, *recipes, recipeMap)
+func createSubmitButton(s common.ScreenInterface, wf common.WorkflowInterface, fr recipe.FileReader, recipes map[string]bool, recipeMap map[string]recipe.Recipe) *widget.Button {
+	b := widget.NewButtonWithIcon("Add To Shopping List", fyne.NewMenuItemSeparator().Icon, func() {
+		selectedRecipes := []string{}
+		for k, v := range recipes {
+			if v {
+				selectedRecipes = append(selectedRecipes, k)
+			}
+		}
+		err := wf.SubmitShoppingList(s, wf, fr, selectedRecipes, recipeMap)
 		if err != nil {
 			log.Println(err)
 		}
 	})
+	b.Importance = widget.HighImportance
+	return b
 }
 
 // NewApp returns a fyne.Window
@@ -67,23 +82,49 @@ func NewApp(recipes []recipe.Recipe, recipeMap map[string]recipe.Recipe, wf comm
 	for _, v := range recipes {
 		recipesAsStrings = append(recipesAsStrings, v.Name)
 	}
-	recipeList := createNewListOfRecipes(recipesAsStrings)
+	selectedRecipe := map[string]bool{}
+	recipeList, selectedRecipe := createNewListOfRecipes(selectedRecipe, recipesAsStrings)
 
-	submit := createSubmitButton(s, wf, fr, &recipeList.Selected, recipeMap)
+	submit := createSubmitButton(s, wf, fr, selectedRecipe, recipeMap)
 	gridTop := container.New(layout.NewGridWrapLayout(fyne.NewSize(screenWidth, progressBarHeight)), label, p)
 	grid := container.New(layout.NewGridWrapLayout(fyne.NewSize(screenWidth, recipeListHeight)), recipeList)
-	gridBottum := container.New(layout.NewGridWrapLayout(fyne.NewSize(screenWidth, progressBarHeight)), submit)
+	gridBottum := container.New(layout.NewGridWrapLayout(fyne.NewSize(screenWidth, submitBarHeight)), submit)
 	masterGrid := container.New(layout.NewVBoxLayout(), gridTop, grid, gridBottum)
 
-	// Set Window and execute
-	myWindow.SetFixedSize(true)
 	myWindow.Resize(fyne.Size{Width: screenWidth, Height: screenHeight})
 	myWindow.SetContent(masterGrid)
 
 	return myWindow
 }
 
-func createNewListOfRecipes(recipesStr []string) *widget.CheckGroup {
-	// Recipe list with all recipes
-	return widget.NewCheckGroup(recipesStr, nil)
+func createNewListOfRecipes(selectedRecipe map[string]bool, recipesStr []string) (*widget.List, map[string]bool) {
+	for _, r := range recipesStr {
+		selectedRecipe[r] = false
+	}
+	l := widget.NewList(
+		func() int {
+			return len(recipesStr)
+		},
+		func() fyne.CanvasObject {
+			hbox := container.NewGridWithColumns(listContainerCols)
+			hbox1 := container.NewGridWithColumns(listCols)
+			hbox1.Add(widget.NewCheck("", func(bool) {}))
+			hbox1.Add(widget.NewLabel("table"))
+			hbox.Add(hbox1)
+			return hbox
+		},
+		func(li widget.ListItemID, o fyne.CanvasObject) {
+			// Update Checkbox
+			listContainer := o.(*fyne.Container).Objects[listBoxIndex].(*fyne.Container)
+			recipeCheckBox := listContainer.Objects[checkIndex].(*widget.Check)
+			recipeCheckBox.Checked = selectedRecipe[recipesStr[li]]
+			recipeCheckBox.OnChanged = func(b bool) {
+				selectedRecipe[recipesStr[li]] = b
+			}
+			recipeCheckBox.Refresh()
+
+			// Update label
+			listContainer.Objects[labelIndex].(*widget.Label).SetText(recipesStr[li])
+		})
+	return l, selectedRecipe
 }
--- pkg/gui/gui_test.go
+++ pkg/gui/gui_test.go
@@ -4,6 +4,7 @@ package gui
 import (
 	"fmt"
 	"go-shopping-list/pkg/common"
+	"go-shopping-list/pkg/fruit"
 	"go-shopping-list/pkg/recipe"
 	workflow "go-shopping-list/pkg/workflows"
 	"testing"
@@ -37,21 +38,30 @@ func TestGuiTest(t *testing.T) {
 }
 
 func (*guiTest) Test_mockFileInfo() {
-	testRecipe := []recipe.Recipe{}
-	_ = NewApp(testRecipe, nil, &workflow.TerminalFakeWorkflow{})
+	testRecipe := []recipe.Recipe{
+		recipe.Recipe{
+			Name: fruit.Avocado,
+		},
+	}
+	_ = NewApp(testRecipe, map[string]recipe.Recipe{fruit.Avocado: {
+		Name: fruit.Avocado,
+	}}, &workflow.TerminalFakeWorkflow{})
 }
 
 func (g *guiTest) Test_buttonPress_Pass() {
-	b := createSubmitButton(g.mockScreen, g.mockWorkflow, g.mockFileReader, &[]string{}, map[string]recipe.Recipe{})
+	recipes := map[string]bool{
+		fruit.Blueberry: true,
+	}
+	b := createSubmitButton(g.mockScreen, g.mockWorkflow, g.mockFileReader, recipes, map[string]recipe.Recipe{})
 	g.mockScreen.On(UpdateProgessBarString, progressBarEmpty)
 	g.mockScreen.On(UpdateProgessBarString, progressBarFull)
 	g.mockScreen.On(UpdateLabelString, recipeFinishLabel)
-	g.mockWorkflow.On("SubmitShoppingList", g.mockScreen, g.mockWorkflow, g.mockFileReader, []string{}, map[string]recipe.Recipe{}).Return(nil)
+	g.mockWorkflow.On("SubmitShoppingList", g.mockScreen, g.mockWorkflow, g.mockFileReader, []string{fruit.Blueberry}, map[string]recipe.Recipe{}).Return(nil)
 	test.Tap(b)
 }
 
 func (g *guiTest) Test_buttonPress_Error() {
-	b := createSubmitButton(g.mockScreen, g.mockWorkflow, g.mockFileReader, &[]string{}, map[string]recipe.Recipe{})
+	b := createSubmitButton(g.mockScreen, g.mockWorkflow, g.mockFileReader, map[string]bool{}, map[string]recipe.Recipe{})
 	g.mockScreen.On(UpdateProgessBarString, progressBarEmpty)
 	g.mockScreen.On(UpdateProgessBarString, progressBarFull)
 	g.mockScreen.On(UpdateLabelString, recipeFinishLabel)
